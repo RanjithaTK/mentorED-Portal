d60d2d11aae85db9563eb64876fde665c45e6533
Ashwini Kumar <ashwini@tunerlabs.com>
2022-11-07 17:56:38 +0530

    grid layout

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.scss"],
 })
 export class AuthComponent implements OnInit {
+  title = "Welcome to mentorED";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      "demo",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        "./app/modules/ath/MentorEd-logo.svg"
+      )
+    );
   }
 
+  ngOnInit(): void {}
 }
