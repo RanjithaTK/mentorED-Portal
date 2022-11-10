22e35e1bc8e16924a87eb9fc688f8f5fd37537cf
GSuma21 <21gsuma@gmail.com>
2022-11-10 10:23:48 +0530

    issue 2150 #feat footer component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CoreModule } from 'src/app/core/core.module';
 @NgModule({
   declarations: [
     HomeComponent,
@@ -12,7 +13,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ]
  
 })
