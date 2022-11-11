6bd5915ba484e7486a605094ea28f54709b8674f
im ranjitha <ranjithatk101@gmail.com>
2022-11-11 10:30:44 +0530

    reverting some unwanted changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,23 @@
-import { NgModule } from "@angular/core"
-import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from "./app-routing.module"
-import { AppComponent } from "./app.component"
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import { SharedModule } from "./shared/shared.module"
-import { CoreModule } from "./core/core.module"
-import { MatCardModule } from "@angular/material/card"
-import { MatButtonModule } from "@angular/material/button"
-import { ReactiveFormsModule } from "@angular/forms"
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { SharedModule } from './shared/shared.module'
+import { CoreModule } from './core/core.module'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    BrowserModule.withServerTransition({ appId: "serverApp" }),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
     CoreModule,
-    MatCardModule,
-    MatButtonModule,
     ReactiveFormsModule,
   ],
   providers: [],
