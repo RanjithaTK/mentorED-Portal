6522232c7eb3f645337b4f8c54745d41629d9587
im ranjitha <ranjithatk101@gmail.com>
2022-11-11 13:59:08 +0530

    changes

--- src/app/modules/auth-routing.module.ts
+++ src/app/modules/auth-routing.module.ts
@@ -1,42 +1,42 @@
-import { NgModule } from "@angular/core"
-import { RouterModule, Routes } from "@angular/router"
-import { AuthComponent } from "./auth/auth.component"
-import { LoginComponent } from "./auth/login/login.component"
-import { RegisterComponent } from "./auth/register/register.component"
-import { RoleSelectionComponent } from "./auth/role-selection/role-selection.component"
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AuthComponent } from './auth/auth.component'
+import { LoginComponent } from './auth/login/login.component'
+import { RegisterComponent } from './auth/register/register.component'
+import { RoleSelectionComponent } from './auth/role-selection/role-selection.component'
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: AuthComponent,
     children: [
       {
-        path: "login",
+        path: 'login',
         component: LoginComponent,
       },
       {
-        path: "register",
+        path: 'register',
         component: RegisterComponent,
       },
       {
-        path: "roleSelection",
+        path: 'roleSelection',
         component: RoleSelectionComponent,
       },
       {
-        path: "",
-        redirectTo: "login",
-        pathMatch: "full",
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
       },
       {
-        path: "**",
-        redirectTo: "/auth/login",
+        path: '**',
+        redirectTo: '/auth/login',
       },
     ],
   },
 ]
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+   imports: [RouterModule.forChild(routes)],
+   exports: [RouterModule]
 })
-export class AuthRoutingModule {}
+export class AuthRoutingModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { MatButtonModule } from "@angular/material/button"
-import { MatInputModule } from "@angular/material/input"
-import { DynamicFormComponent } from "./components"
-import { ReactiveFormsModule } from "@angular/forms"
-import { RoleSelectionCardComponent } from "./components/role-selection-card/role-selection-card.component"
-import { MatCardModule } from "@angular/material/card"
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { MatButtonModule } from '@angular/material/button'
+import { MatInputModule } from '@angular/material/input'
+import { DynamicFormComponent } from './components'
+import { ReactiveFormsModule } from '@angular/forms'
+import { RoleSelectionCardComponent } from './components/role-selection-card/role-selection-card.component'
+import { MatCardModule } from '@angular/material/card'
 @NgModule({
   declarations: [DynamicFormComponent, RoleSelectionCardComponent],
   imports: [
@@ -22,4 +22,4 @@ import { MatCardModule } from "@angular/material/card"
     RoleSelectionCardComponent,
   ],
 })
-export class SharedModule {}
+export class SharedModule { }
