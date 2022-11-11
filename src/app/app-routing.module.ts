e8725b9c5638685e9c04ada8ed56c77c14c85f5d
im ranjitha <ranjithatk101@gmail.com>
2022-11-11 16:21:01 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
   {
     path: 'auth',
@@ -16,7 +15,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./modules/layout/layout.module').then(m => m.LayoutModule)
   },
- 
 ];
 
 @NgModule({
