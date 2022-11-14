97bc5e7b76e2a3031163824cf682176bc576b2e7
im ranjitha <ranjithatk101@gmail.com>
2022-11-14 12:02:04 +0530

    changed roleSelection to role-selection in routing

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
                 component: RegisterComponent
             },
             {
-                path: 'roleSelection',
+                path: 'role-selection',
                 component: RoleSelectionComponent
             },
             {
