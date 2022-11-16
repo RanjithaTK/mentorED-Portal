6688782a36e6c2d9197f39805771ea13b9920b1c
Cafnanc <cafnanc@gmail.com>
2022-11-16 11:26:52 +0530

    star rating component

--- src/app/shared/components/star-rating/star-rating.component.ts
+++ src/app/shared/components/star-rating/star-rating.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import * as _ from 'lodash-es';
 
 @Component({
   selector: 'app-star-rating',
@@ -9,36 +9,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
-      multi:true,
-      useExisting: StarRatingComponent
-    }
-  ]
+      multi: true,
+      useExisting: StarRatingComponent,
+    },
+  ],
 })
-export class StarRatingComponent implements OnInit {
+export class StarRatingComponent implements OnInit, ControlValueAccessor {
+  @Input() rate=0;
+  @Input() numberOfStars: any;
+  @Input() label: any;
+  range: any;
+  rating: Set<unknown>;
+  touched: boolean = false;
 
-  @Input('rating') rating: number | string;
-  @Input('starCount') starCount: number = 5;
+  constructor() { }
 
-  snackBarDuration: number = 2000;
-  ratingArr:Array<any>=[];
-  touched: boolean=false;
-  disabled: boolean=false
-
-  constructor(private snackBar: MatSnackBar) {
+  ngOnInit() {
+    this.range = [];
+    _.range(0, this.numberOfStars).forEach((range) => {
+      this.range.push(range);
+    });
   }
 
-  onChange = (quantity:any) => {};
+  onChange = (quantity:any) => { };
 
-  onTouched = () => {};
+  onTouched = () => { };
 
-  writeValue(value:any) {
+  writeValue(quantity: number) {
+    this.rate = quantity;
   }
-  
-  registerOnChange(onChange: any) {
+
+  registerOnChange(onChange: any): void {
     this.onChange = onChange;
   }
 
-  registerOnTouched(onTouched: any) {
+  registerOnTouched(onTouched: any): void {
     this.onTouched = onTouched;
   }
 
@@ -48,31 +53,13 @@ export class StarRatingComponent implements OnInit {
       this.touched = true;
     }
   }
-
-  setDisabledState(isDisabled: boolean) {
-    this.disabled = isDisabled;
-  }
-
-
-  ngOnInit() {
-    for (let index = 0; index < this.starCount; index++) {
-      this.ratingArr.push(index);
-    }
-  }
-  onClick(rating:number) {
-    this.markAsTouched();
-    this.snackBar.open('You rated ' + rating + ' / ' + this.starCount, '', {
-      duration: this.snackBarDuration
-    });
-    this.rating = rating;
-    this.onChange(this.rating)
+  
+  onRate(rate:any) {
+    this.rate = rate;
+    this.onChange(this.rate);
   }
 
   showIcon(index:number) {
-    if (this.rating && this.rating >= index + 1) {
-      return 'star';
-    } else {
-      return 'star_border';
-    }
+    return (this.rate && this.rate >= index + 1) ? 'star' : 'star_border';
   }
 }
