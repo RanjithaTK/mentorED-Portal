ee5576a7a4bf0eab509f785883b82bc6ec224ad5
im ranjitha <ranjithatk101@gmail.com>
2022-11-28 11:56:28 +0530

    added translate in role-selection

--- src/app/modules/auth/role-selection/role-selection.component.ts
+++ src/app/modules/auth/role-selection/role-selection.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core"
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: "app-role-selection",
   templateUrl: "./role-selection.component.html",
@@ -11,13 +12,13 @@ export class RoleSelectionComponent implements OnInit {
 
   roleList = [
     {
-      role: "I'm a Mentor",
-      description: "Build confidence as a leader, and grow your network.",
+      role: "MENTOR",
+      description: "MENTOR_DESCRIPTION",
       image: "../../../../assets/images/role-selection/mentor_icon.png",
     },
     {
-      role: "I'm a Mentee",
-      description: "Learn and grow from verified Mentors",
+      role: "MENTEE",
+      description: "MENTEE_DESCRIPTION",
       image: "../../../../assets/images/role-selection/mentee_icon.png",
     },
   ]
