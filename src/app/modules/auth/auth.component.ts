25361f3d1ae126017cb8588c9bdc63e569d58c4a
kiran haridas <kiranharidas187@gmail.com>
2022-11-29 19:21:08 +0530

    Remember me changes done, UI enhancements

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,13 +1,16 @@
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
   styleUrls: ["./auth.component.scss"],
 })
 export class AuthComponent implements OnInit {
-
-  constructor() {
+isPhone: boolean;
+  constructor( private responsive: BreakpointObserver) { 
+    this.responsive.observe(Breakpoints.Handset).subscribe(result => {
+      this.isPhone = result.matches
+    })
   }
 
   ngOnInit(): void {}
