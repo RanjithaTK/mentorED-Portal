7c1aa2cd0c85feec281cd0ecebeac08d88c6163d
Cafnanc <cafnanc@gmail.com>
2022-11-30 13:43:31 +0530

    Auth module left side div headers

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,4 +1,7 @@
+import { Location } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: "app-auth",
@@ -6,8 +9,36 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./auth.component.scss"],
 })
 export class AuthComponent implements OnInit {
+  header: any;
+  subHeader: any;
 
-  constructor() {
+  constructor(private router: Router, private location: Location) {
+    router.events.pipe(
+      filter((event:any) => event instanceof NavigationEnd)
+    ).subscribe(data=>{
+      let key = data.url.split('?')[0].split('/').pop()
+      switch (key) {
+        case 'login':
+          this.header = "LOGIN_HEADER"
+          this.subHeader = "LOGIN_SUB_HEADER"
+          break;
+        case 'register':
+          this.header = "SIGNUP_HEADER"
+          this.subHeader = "SIGNUP_SUB_HEADER"
+          break;
+        case 'role-selection':
+          this.header = "CHOOSE_YOUR_ROLE"
+          this.subHeader = "JOIN_THE_COMMUNITY"
+          break;
+        case 'otp':
+          this.header = "VERIFY_ACCOUNT"
+          this.subHeader = "OTP_SUB_TEXT"
+          break;
+
+        default:
+          break;
+      }
+    })
   }
 
   ngOnInit(): void {}
