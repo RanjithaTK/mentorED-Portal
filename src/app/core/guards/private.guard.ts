c4f3b2ed9718c8dc330b2d7523aa11ca5af54999
Cafnanc <cafnanc@gmail.com>
2022-12-01 18:06:43 +0530

    private guard - token from user service

--- src/app/core/guards/private.guard.ts
+++ src/app/core/guards/private.guard.ts
@@ -2,24 +2,29 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { localKeys } from '../constants/localStorage.keys';
 import { LocalStorageService } from '../services/local-storage/local-storage.service';
+import { UserService } from '../services/user/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrivateGuard implements CanActivate {
 
-  constructor(private router: Router, private localStorage: LocalStorageService) { }
+  constructor(private router: Router, private localStorage: LocalStorageService, private userService: UserService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot){
-      return this.localStorage.getLocalData(localKeys.TOKEN).then((token: any) => {
-        if(token){
-          return true
-        } else {
-          this.router.navigate(['/auth/login'])
-          return false
-        }
-      })
+      if(this.userService.token){
+        return true
+      } else {
+        return this.localStorage.getLocalData(localKeys.TOKEN).then((token: any) => {
+          if(token){
+            return true
+          } else {
+            this.router.navigate(['/auth/login'])
+            return false
+          }
+        })
+      }
     }
 }
