5a96907edf217609ab93ee73e4e93b9031a7ed1d
Ashwini Kumar <ashwini@tunerlabs.com>
2022-12-02 21:37:17 +0530

    signup and otp routing

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
 
     async createAccount(formData: any) {
       const config = {
-        url: API_CONSTANTS.REGISTRATION_OTP,
+        url: API_CONSTANTS.CREATE_ACCOUNT,
         payload: formData,
       };
       try {
--- src/app/core/services/profile/profile.service.ts
+++ src/app/core/services/profile/profile.service.ts
@@ -1,51 +1,61 @@
-import { Location } from '@angular/common';
-import { Injectable } from '@angular/core';
-import * as _ from 'lodash';
-import { map } from 'rxjs';
-import { API_CONSTANTS } from '../../constants/apiUrlConstants';
-import { localKeys } from '../../constants/localStorage.keys';
-import { ApiService } from '../api/api.service';
-import { LocalStorageService } from '../local-storage/local-storage.service';
-import { UserService } from '../user/user.service';
+import { Location } from "@angular/common";
+import { Injectable } from "@angular/core";
+import * as _ from "lodash";
+import { map } from "rxjs";
+import { API_CONSTANTS } from "../../constants/apiUrlConstants";
+import { localKeys } from "../../constants/localStorage.keys";
+import { ApiService } from "../api/api.service";
+import { LocalStorageService } from "../local-storage/local-storage.service";
+import { UserService } from "../user/user.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProfileService {
-
-  constructor(private localStorage: LocalStorageService, private apiService: ApiService, private userService: UserService, private _location: Location) { }
+  constructor(
+    private localStorage: LocalStorageService,
+    private apiService: ApiService,
+    private userService: UserService,
+    private _location: Location
+  ) {}
 
   async profileDetails(): Promise<any> {
     return new Promise((resolve) => {
       try {
-        this.localStorage.getLocalData(localKeys.USER_DETAILS)
+        this.localStorage
+          .getLocalData(localKeys.USER_DETAILS)
           .then(async (data) => {
             if (data) {
               resolve(data);
             } else {
               var res = await this.getProfileDetailsAPI();
-              await this.localStorage.saveLocalData(localKeys.USER_DETAILS, JSON.stringify(res));
-              data = _.get(data, 'user');
+              await this.localStorage.saveLocalData(
+                localKeys.USER_DETAILS,
+                JSON.stringify(res)
+              );
+              data = _.get(data, "user");
               resolve(data);
             }
-          })
-      } catch (error) {
-      }
+          });
+      } catch (error) {}
     });
   }
 
   getProfileDetailsAPI() {
     const config = {
       url: API_CONSTANTS.PROFILE_DETAILS,
-      payload: {}
+      payload: {},
     };
     return this.apiService.get(config).pipe(
       map((result: any) => {
-        let data = _.get(result, 'result');
-        this.localStorage.saveLocalData(localKeys.USER_DETAILS, JSON.stringify(data));
+        let data = _.get(result, "result");
+        this.localStorage.saveLocalData(
+          localKeys.USER_DETAILS,
+          JSON.stringify(data)
+        );
         return data;
       })
-    )
+    );
   }
 
   profileUpdate(formData: any) {
@@ -56,11 +66,26 @@ export class ProfileService {
     return this.apiService.post(config).pipe(
       map(async (response: any) => {
         let profileData = await this.getProfileDetailsAPI();
-        await this.localStorage.saveLocalData(localKeys.USER_DETAILS, JSON.stringify(profileData));
+        await this.localStorage.saveLocalData(
+          localKeys.USER_DETAILS,
+          JSON.stringify(profileData)
+        );
         this.userService.userEvent.next(profileData);
         this._location.back();
         return profileData;
       })
-    )
+    );
+  }
+  registrationOtp(formData: any) {
+    const config = {
+      url: API_CONSTANTS.REGISTRATION_OTP,
+      payload: formData,
+    };
+    console.log('config : ',config)
+    return this.apiService.post(config).pipe(
+      map((result: any) => {
+        return result;
+      })
+    );
   }
 }
