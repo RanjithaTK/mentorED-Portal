6fd1a9b17cc3433b9794f893d85dd65c2bb160e2
im ranjitha <ranjithatk101@gmail.com>
2022-12-12 11:15:19 +0530

    mentor profile generic detail component

--- src/app/core/services/profile/profile.service.ts
+++ src/app/core/services/profile/profile.service.ts
@@ -1,13 +1,12 @@
-import { Location } from "@angular/common";
-import { Injectable } from "@angular/core";
-import * as _ from "lodash";
-import { map } from "rxjs";
-import { API_CONSTANTS } from "../../constants/apiUrlConstants";
-import { localKeys } from "../../constants/localStorage.keys";
-import { ApiService } from "../api/api.service";
-import { LocalStorageService } from "../local-storage/local-storage.service";
-import { ToastService } from "../toast.service";
-import { UserService } from "../user/user.service";
+import { Location } from '@angular/common'
+import { Injectable } from '@angular/core'
+import * as _ from 'lodash'
+import { map } from 'rxjs'
+import { API_CONSTANTS } from '../../constants/apiUrlConstants'
+import { localKeys } from '../../constants/localStorage.keys'
+import { ApiService } from '../api/api.service'
+import { LocalStorageService } from '../local-storage/local-storage.service'
+import { UserService } from '../user/user.service'
 
 @Injectable({
   providedIn: 'root',
@@ -18,8 +17,6 @@ export class ProfileService {
     private apiService: ApiService,
     private userService: UserService,
     private _location: Location,
-    private toastService: ToastService,
-    private toast: ToastService
   ) {}
 
   async profileDetails(): Promise<any> {
@@ -28,13 +25,18 @@ export class ProfileService {
         this.localStorage
           .getLocalData(localKeys.USER_DETAILS)
           .then(async (data) => {
-            this.getProfileDetailsAPI().subscribe(async (res) => {
+            if (data) {
+              resolve(JSON.parse(data))
+            } else {
+              var res = await this.getProfileDetailsAPI()
+              console.log(res)
               await this.localStorage.saveLocalData(
                 localKeys.USER_DETAILS,
                 JSON.stringify(res),
               )
-              resolve(res)
-            })
+              data = _.get(data, 'user')
+              resolve(data)
+            }
           })
       } catch (error) {}
     })
@@ -79,23 +81,12 @@ export class ProfileService {
     const config = {
       url: API_CONSTANTS.REGISTRATION_OTP,
       payload: formData,
-    };
+    }
+    console.log('config : ', config)
     return this.apiService.post(config).pipe(
       map((result: any) => {
         return result
       }),
     )
-  }
-  generateOtp(formData: any) {
-    const config = {
-      url: API_CONSTANTS.GENERATE_OTP,
-      payload: formData,
-    };
-    return this.apiService.post(config).pipe(
-      map((result: any) => {
-        this.toast.showMessage(result.message, "success");
-        return result;
-      })
-    );
   }
 }
