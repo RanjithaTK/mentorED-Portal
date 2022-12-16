502fb38e2850cd47b4d1ed35089b99bd9c2ed304
im ranjitha <ranjithatk101@gmail.com>
2022-12-16 13:50:36 +0530

    added enrolling and joining api

--- src/app/shared/components/session-card/session-card.component.ts
+++ src/app/shared/components/session-card/session-card.component.ts
@@ -1,39 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { localKeys } from 'src/app/core/constants/localStorage.keys';
-import { LocalStorageService } from 'src/app/core/services/local-storage/local-storage.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { localKeys } from 'src/app/core/constants/localStorage.keys'
+import { LocalStorageService } from 'src/app/core/services/local-storage/local-storage.service'
 import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-session-card',
   templateUrl: './session-card.component.html',
-  styleUrls: ['./session-card.component.scss']
+  styleUrls: ['./session-card.component.scss'],
 })
 export class SessionCardComponent implements OnInit {
-  @Input() cardData: any;
-  buttonConfig:any;
-  isCreator: boolean;
-  userData: any;
-  constructor(private localStorage:LocalStorageService,private translate: TranslateService) { }
+  @Input() cardData: any
+  @Output() buttonIsToggle = new EventEmitter()
+  buttonConfig: any
+  isCreator: boolean
+  userData: any
+  constructor(
+    private localStorage: LocalStorageService,
+    private translate: TranslateService,
+  ) {}
 
   async ngOnInit() {
-    this.isCreator = await this.checkIfCreator();
-    this.setButtonConfig(this.isCreator);
+    this.isCreator = await this.checkIfCreator()
+    this.setButtonConfig(this.isCreator)
   }
 
   setButtonConfig(isCreator: boolean) {
-    if(isCreator){
-      this.buttonConfig={label:"START",type:"startAction"};
+    if (isCreator) {
+      this.buttonConfig = { label: 'START', type: 'startAction' }
     } else {
-      this.buttonConfig=(!isCreator&&this.cardData.isEnrolled || !isCreator&&this.cardData.sessionId)?{label:"JOIN",type:"joinAction"}:{label:"ENROLL",type:"enrollAction"};
+      this.buttonConfig =
+        (!isCreator && this.cardData.isEnrolled) ||
+        (!isCreator && this.cardData.sessionId)
+          ? { label: 'JOIN', type: 'joinAction' }
+          : { label: 'ENROLL', type: 'enrollAction' }
     }
-    let currentTimeInSeconds=Math.floor(Date.now()/1000);
-    this.buttonConfig.isEnabled = (this.cardData.startDate-currentTimeInSeconds<300)?true:false;
+    let currentTimeInSeconds = Math.floor(Date.now() / 1000)
+    this.buttonConfig.isEnabled =
+      this.cardData.startDate - currentTimeInSeconds < 300 ? true : false
   }
 
   async checkIfCreator() {
     this.userData = await this.localStorage.getLocalData(localKeys.USER_DETAILS)
-    return (this.cardData.userId == this.userData._id) ?true : false;
+    return this.cardData.userId == this.userData._id ? true : false
   }
- 
 
+  buttonToggle(action: any, data: any) {
+    let detail: any = { action: action, data: data }
+    this.buttonIsToggle.emit(detail)
+  }
 }
