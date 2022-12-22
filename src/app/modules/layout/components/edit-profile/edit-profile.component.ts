d4960f0fd2e90d98a1cdc1095cb74b6be25d0418
im ranjitha <ranjithatk101@gmail.com>
2022-12-22 12:49:58 +0530

    ui changes in edit profile

--- src/app/modules/layout/components/edit-profile/edit-profile.component.ts
+++ src/app/modules/layout/components/edit-profile/edit-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { EDIT_PROFILE_FORM } from 'src/app/core/constants/formConstant';
+import { FormService } from 'src/app/core/services/form/form.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,11 +8,14 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent implements OnInit {
+  formData: any;
   
 
-  constructor() {}
+  constructor(private formService:FormService) {}
 
   ngOnInit(): void {
-  
+    this.formService.getForm(EDIT_PROFILE_FORM).subscribe((form)=>{
+        this.formData=form
+    })
   }
 }
