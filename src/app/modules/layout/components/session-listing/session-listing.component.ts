af4c84f6a2d061b64762b6436607122a0e5ddffe
Cafnanc <cafnanc@gmail.com>
2022-12-28 16:57:23 +0530

    edits

--- src/app/modules/layout/components/session-listing/session-listing.component.ts
+++ src/app/modules/layout/components/session-listing/session-listing.component.ts
@@ -2,22 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/core/services';
 import { API_CONSTANTS } from 'src/app/core/constants/apiUrlConstants'
-<<<<<<< HEAD
 import { FormService } from 'src/app/core/services/form/form.service';
 import { CREATE_SESSION_FORM, EDIT_PROFILE_FORM, FAQ, HELP_VIDEOS, TERMS_AND_CONDITIONS_FORM } from 'src/app/core/constants/formConstant';
 import { map } from 'rxjs';
 import { DbService } from 'src/app/core/services/db/db.service';
 import { SessionService } from 'src/app/core/services/session/session.service';
 import { LocalStorageService } from 'src/app/core/services/local-storage/local-storage.service';
-
-=======
-import { SessionService } from 'src/app/core/services/session/session.service';
 import { localKeys } from 'src/app/core/constants/localStorage.keys';
-import { LocalStorageService } from 'src/app/core/services/local-storage/local-storage.service';
+
 interface item {
   userId?: string;
 }
->>>>>>> upstream/release-1.0.0
 
 @Component({
   selector: 'app-session-listing',
@@ -46,9 +41,7 @@ export class SessionListingComponent implements OnInit {
   async ngOnInit(){
     this.userDetails= JSON.parse( await this.localStorage.getLocalData(localKeys.USER_DETAILS))
     this.cardHeading = (this.selectedPage == '/enrolled-sessions') ? "MY_SESSIONS" : "ALL_SESSIONS";
-    this.getAllSession().subscribe((sessions)=>{
-      this.cardDetails = sessions;
-    })
+    this.getAllSession().subscribe();
   }
 
   onClickViewMore() {
@@ -59,27 +52,18 @@ export class SessionListingComponent implements OnInit {
       url: API_CONSTANTS.HOME_SESSION + this?.page + '&limit=' + this?.limit
     };
     this.loading = true;
-<<<<<<< HEAD
     return this.apiService.get(config).pipe(
       map(((data: any) => {
         this.loading = false;
-        return (this.selectedPage == '/enrolled-sessions') ? data.result.mySessions : data.result.allSessions;
+        this.cardDetails =  (this.selectedPage == '/enrolled-sessions') ? data.result.mySessions : data.result.allSessions;
+        if (!this.cardDetails.length) {
+          this.noData = (this.selectedPage == '/enrolled-sessions') ? "NO_ENROLL_SESSION_CONTENT"  : "NO_ALL_SESSION_CONTENT";
+        }
+        this.sessionsCount=this.cardDetails.length
       }))
     )
-=======
-    this.apiService.get(config).subscribe((data: any) => {
-      this.loading = false;
-      this.cardDetails = (this.selectedPage == '/enrolled-sessions') ? data.result.mySessions : data.result.allSessions;
-      if (!this.cardDetails.length) {
-        this.noData = (this.selectedPage == '/enrolled-sessions') ? "NO_ENROLL_SESSION_CONTENT"  : "NO_ALL_SESSION_CONTENT";
-      }
-      this.sessionsCount=this.cardDetails.length
-    }, error => {
-      this.loading = false;
-    })
-
->>>>>>> upstream/release-1.0.0
   }
+  
   buttonClick(event:any){
     switch(event.action.type){
       case "enrollAction":
