0d270bdccf8fa15e69d7a7e43f26cc4d8724d443
GSuma21 <21gsuma@gmail.com>
2022-12-29 11:23:50 +0530

    issue #feat edit profile api integration

--- src/app/modules/layout/components/edit-profile/edit-profile.component.ts
+++ src/app/modules/layout/components/edit-profile/edit-profile.component.ts
@@ -5,6 +5,9 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { ExitPopupComponent } from '../../../../shared/components/exit-popup/exit-popup.component';
 import { DynamicFormComponent } from 'src/app/shared';
+import { ProfileService } from 'src/app/core/services/profile/profile.service';
+import { LocalStorageService } from 'src/app/core/services/local-storage/local-storage.service';
+import { localKeys } from 'src/app/core/constants/localStorage.keys';
 
 
 @Component({
@@ -15,11 +18,14 @@ import { DynamicFormComponent } from 'src/app/shared';
 export class EditProfileComponent implements OnInit {
   formData: any;
   canLeave:any = false;
+  userDetails:any
+  user = this.localStorage.getLocalData(localKeys.USER_DETAILS);
   @ViewChild('editProfile')editProfile:DynamicFormComponent;
 
-  constructor(private formService:FormService,private router:Router,public dialog: MatDialog) {}
+  constructor(private formService:FormService,private router:Router,public dialog: MatDialog,private profileService: ProfileService,private localStorage:LocalStorageService) {}
   
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.userDetails = JSON.parse(await this.user)
     this.formService.getForm(EDIT_PROFILE_FORM).subscribe((form)=>{
         this.formData=form
     })
@@ -47,4 +53,10 @@ export class EditProfileComponent implements OnInit {
     
   }
 
+  onSubmit(){
+    this.profileService.profileUpdate(this.editProfile.myForm.value).subscribe((result:any) =>{
+      console.log(result)
+    }) 
+    // this.router.navigate(['/profile'])
+  }
 }
