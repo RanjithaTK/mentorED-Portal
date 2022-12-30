6a624dc2cca3d9b7b411d7419d5d1383f112c9ae
im ranjitha <ranjithatk101@gmail.com>
2022-12-30 14:34:21 +0530

    changes

--- src/app/modules/layout/components/session-listing/session-listing.component.ts
+++ src/app/modules/layout/components/session-listing/session-listing.component.ts
@@ -3,7 +3,13 @@ import { Router } from '@angular/router';
 import { ApiService } from 'src/app/core/services';
 import { API_CONSTANTS } from 'src/app/core/constants/apiUrlConstants'
 import { FormService } from 'src/app/core/services/form/form.service';
-import { CREATE_SESSION_FORM, EDIT_PROFILE_FORM, FAQ, HELP_VIDEOS, TERMS_AND_CONDITIONS_FORM } from 'src/app/core/constants/formConstant';
+import {
+  CREATE_SESSION_FORM,
+  EDIT_PROFILE_FORM,
+  FAQ,
+  HELP_VIDEOS,
+  TERMS_AND_CONDITIONS_FORM,
+} from 'src/app/core/constants/formConstant'
 import { map } from 'rxjs';
 import { DbService } from 'src/app/core/services/db/db.service';
 import { SessionService } from 'src/app/core/services/session/session.service';
@@ -27,49 +33,67 @@ export class SessionListingComponent implements OnInit {
   start: any = 0;
   lastIndex: any = 4;
   selectedPage: any;
-  page: any = 1;
+  page: any;
   limit: any = 4;
-  noData: any="NO_ALL_SESSION_CONTENT"
+  noData: any
   loading: boolean = false;
   userDetails: any;
   sessionsCount = 0
-  allSession: any = []
   showLoadMoreButton: boolean = false
   dataCount = 0
-  type: any
+  isEnrolledSessions: any
 
-  
-  constructor(private router: Router, private apiService: ApiService, private form: FormService, private sessionService: SessionService,private localStorage:LocalStorageService) {
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+    private form: FormService,
+    private sessionService: SessionService,
+    private localStorage:LocalStorageService,
+  ) {
     this.selectedPage = router.url
   }
 
-  async ngOnInit(){
-    this.userDetails= JSON.parse( await this.localStorage.getLocalData(localKeys.USER_DETAILS))
-    this.cardHeading = (this.selectedPage == '/enrolled-sessions') ? "MY_SESSIONS" : "ALL_SESSIONS";
-    this.getAllSession()
+  ngOnInit(){
+    this.page = 1
+    this.localStorage
+      .getLocalData(localKeys.USER_DETAILS)
+      .then((userDetails) => {
+        this.userDetails = userDetails
+        this.getAllSession().subscribe()
+      })
+    this.cardHeading =
+      this.selectedPage == '/enrolled-sessions' ? 'MY_SESSIONS' : 'ALL_SESSIONS'
   }
 
   onClickViewMore() {
     this.page = this.page + 1
-    this.getAllSession()
+    this.getAllSession().subscribe()
   }
   getAllSession() {
     this.loading = true
-    let type = this.type == 'enrolled-sessions' ? true : false
+    this.isEnrolledSessions =
+      this.selectedPage == '/enrolled-sessions' ? true : false
     let obj = {
-      type: type,
+      enrolled: this.isEnrolledSessions,
       page: this.page,
       limit: this.limit,
     }
-    this.sessionService.allSession(obj).subscribe((data: any) => {
-      this.cardDetails = this.cardDetails.concat(data?.result[0]?.data)
-      if (!this.cardDetails.length) {
-        this.noData = (this.selectedPage == '/enrolled-sessions') ? "NO_ENROLL_SESSION_CONTENT"  : "NO_ALL_SESSION_CONTENT";
-      }
-      this.allSession = this.allSession.concat(data?.result[0]?.data)
-      this.sessionsCount = data?.result[0]?.count
-      this.showLoadMoreButton = this.allSession?.length === this.sessionsCount ? false : true
-    })
+    return this.sessionService.allSession(obj).pipe(
+      map((data: any) => {
+        this.cardDetails = this.cardDetails.concat(data?.result[0]?.data)
+        if (!this.cardDetails.length) {
+          this.noData =
+            this.selectedPage == '/enrolled-sessions'
+              ? 'NO_ENROLL_SESSION_CONTENT'
+              : 'NO_ALL_SESSION_CONTENT'
+        }
+        this.loading = false
+
+        this.sessionsCount = data?.result[0]?.count
+        this.showLoadMoreButton =
+          this.cardDetails?.length === this.sessionsCount ? false : true
+      }),
+    )
   }
   buttonClick(event: any) {
     switch (event.action.type) {
@@ -77,7 +101,7 @@ export class SessionListingComponent implements OnInit {
         this.sessionService
           .enrollSession(event.data._id)
           .subscribe((result) => {
-            this.getAllSession()
+            this.getAllSession().subscribe()
           })
         break
       case 'joinAction':
