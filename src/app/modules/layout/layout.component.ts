6ad18d6f46c86ab868575c27aa721e2cbe761eeb
GSuma21 <21gsuma@gmail.com>
2023-01-12 16:31:10 +0530

    issue #fix resolved 564

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { Profiler } from 'inspector';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { ProfileService } from 'src/app/core/services/profile/profile.service';
 
 @Component({
   selector: 'app-layout',
@@ -11,7 +14,7 @@ export class LayoutComponent implements OnInit {
 
   options=[{label:"English",value:"en"},{label:"Hindi",value:"hi"}]
   selectedLanguage="en"
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,private profileService:ProfileService,private router: Router) { }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
@@ -19,5 +22,18 @@ export class LayoutComponent implements OnInit {
 
    onLogout(){
     this.authService.logoutAccount()
+  }
+
+  goToProfile() {
+    this.getDetails().then((userDetails)=>{
+      if(userDetails.about){
+        this.router.navigate(['/profile']);
+      }else{
+         this.router.navigate(['/edit-profile'])
+      }
+    })
+  }
+  async getDetails() {
+    return await this.profileService.profileDetails()
   }
 }
