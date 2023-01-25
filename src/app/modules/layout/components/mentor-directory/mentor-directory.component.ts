dcb1a768dcc5c054434ed8c51b15fdcb0e6f3c28
GSuma21 <21gsuma@gmail.com>
2023-01-25 11:31:07 +0530

    issue #feat mentor-directory

--- src/app/modules/layout/components/mentor-directory/mentor-directory.component.ts
+++ src/app/modules/layout/components/mentor-directory/mentor-directory.component.ts
@@ -11,10 +11,12 @@ import { ApiService } from '../../../../core/services/api/api.service'
   styleUrls: ['./mentor-directory.component.scss']
 })
 export class MentorDirectoryComponent implements OnInit {
-  page: any;
-  limit: any;
+  page:any =  2;
+  limit:any =  1000;
   mentors:any = [];
   mentorsCount:any;
+  selectedAlphabet:any = "All"
+  alphabetsArray:any = ["All","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   constructor(private mentorService:MentorService, private apiService:ApiService,private router: Router) { }
 
   ngOnInit(): void {
@@ -27,10 +29,22 @@ export class MentorDirectoryComponent implements OnInit {
     }
     return this.mentorService.getMentorDirectory(obj).pipe(
       map((data: any) => {
-        this.mentors = this.mentors.concat(data.result.data);
-      this.mentorsCount = data.result.count;
+        if(this.selectedAlphabet == "All"){
+          // this.mentors = this.mentors.concat(data.result.data);
+          this.mentors = data.result.data;
+          this.mentorsCount = data.result.count;
+        }else{
+          data.result.data.forEach((ele:any) => {
+            if(ele.key == this.selectedAlphabet){
+              this.mentors = {}
+              this.mentors = ele;
+              console.log(this.mentors)
+            }
+          });
+          
+        }
+        
       }))
-    
   }
   eventAction(event:any) {
     switch (event.type) {
@@ -38,5 +52,10 @@ export class MentorDirectoryComponent implements OnInit {
         this.router.navigate(["/mentor-profile"],{ state: { mentorID: event.data._id } })
         break;
     }
+  }
+  onClickAlphabet(a:any){
+console.log(a)
+this.selectedAlphabet = a;
+this.getMentor().subscribe()
   }
 }
