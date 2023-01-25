12200b305c88b0333f4884672242ff80fb1f4f90
im ranjitha <ranjithatk101@gmail.com>
2023-01-25 11:11:06 +0530

    added dashboard

--- src/app/modules/layout/components/dashboard/dashboard.component.ts
+++ src/app/modules/layout/components/dashboard/dashboard.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { result } from 'lodash-es';
+import { map } from 'rxjs';
+import { API_CONSTANTS } from 'src/app/core/constants/apiUrlConstants';
+import { localKeys } from 'src/app/core/constants/localStorage.keys';
+import { ApiService } from 'src/app/core/services';
+import { LocalStorageService } from 'src/app/core/services/local-storage/local-storage.service';
+import { ProfileService } from 'src/app/core/services/profile/profile.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +14,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  segment: any="mentee"
+  dataAvailable: any
+  isMentor:boolean=false
+  selectedFilter = "WEEKLY";
+  filters: any = [
+    {
+      key: 'WEEKLY',
+      value: 'WEEKLY'
+    },
+    {
+      key: 'MONTHLY',
+      value: 'MONTHLY'
+    },
+    {
+      key: 'QUARTERLY',
+      value: 'QUARTERLY'
+    }
+  ];
+  buttonConfig:any=[
+    {label:"MENTOR_LABEL",value:"mentor"},
+    {label:"MENTEE_LABEL",value:"mentee"}
+  ]
+  loading: boolean = false;
+  data:any;
+  chartData: any = {
+    chart: {
+      data: {
+        labels: [],
+        datasets: [
+          {
+            data: [],
+            backgroundColor: ['#ffab00', '#BEBEBE']
+          }
+        ]
+      }
+    }
+  };
+  constructor(private translate:TranslateService,private profileService:ProfileService,private apiService:ApiService, private localStorage:LocalStorageService) { }
+  
 
   ngOnInit(): void {
+    this.localStorage.getLocalData(localKeys.USER_DETAILS).then((user:any)=>{
+      this.segment = JSON.parse(user).isAMentor?"mentor":this.segment
+      this.isMentor=JSON.parse(user).isAMentor
+      this.getReports().subscribe((result:any)=>{
+        console.log(result)
+      })
+    })
   }
 
+  getReports() {
+    const url = this.segment === 'mentor' ? API_CONSTANTS.MENTOR_REPORTS : API_CONSTANTS.MENTEE_REPORTS;
+    const config = {
+      url: url+this.selectedFilter.toUpperCase(),
+    };
+    return this.apiService.get(config).pipe(
+      map((result:any)=>{
+        console.log(result) 
+        console.log(result.result) 
+        let chartObj;
+          this.chartData.chart.data.labels.length = 0;
+          this.chartData.chart.data.datasets[0].data.length = 0;
+        if(this.segment === 'mentor'){
+          this.chartData.chart.data.labels.push("Total Sessions Created", "Total Sessions Hosted")
+          this.chartData.chart.data.datasets[0].data.push(result.result.totalSessionCreated || 0, result.result.totalsessionHosted || 0);
+        } else {
+          this.chartData.chart.data.labels.push("Total Sessions Enrolled", "Total Sessions Attended")
+          this.chartData.chart.data.datasets[0].data.push(result.result.totalSessionEnrolled || 0, result.result.totalsessionsAttended || 0);
+        }
+        this.dataAvailable=(this.chartData.chart.data.datasets[0].data[0]==0&&this.chartData.chart.data.datasets[0].data[1]==0) ? false:true;
+        this.loading = false;
+      }))
+    
+  }
+  buttonClick(button:any){
+    
+    this.segment=button.value
+    this.getReports().subscribe()
+  }
 }
