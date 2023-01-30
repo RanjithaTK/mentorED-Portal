e0f3e5fe3c6863e297d4b2711081cee0016a3212
GSuma21 <21gsuma@gmail.com>
2023-01-30 11:42:24 +0530

    issue #feat mentor profile page with upcoming session

--- src/app/modules/layout/components/mentor-directory/mentor-directory.component.ts
+++ src/app/modules/layout/components/mentor-directory/mentor-directory.component.ts
@@ -11,13 +11,14 @@ import { ApiService } from '../../../../core/services/api/api.service'
   styleUrls: ['./mentor-directory.component.scss']
 })
 export class MentorDirectoryComponent implements OnInit {
-  page:any =  2;
-  limit:any =  1000;
+  page:any = 1;
+  limit:any = 1000;
   mentors:any = [];
   mentorsCount:any;
   selectedAlphabet:any = "All"
+  selectedMentors:any;
   alphabetsArray:any = ["All","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  constructor(private mentorService:MentorService, private apiService:ApiService,private router: Router) { }
+  constructor(private mentorService:MentorService, private apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
     this.getMentor().subscribe()
@@ -29,33 +30,27 @@ export class MentorDirectoryComponent implements OnInit {
     }
     return this.mentorService.getMentorDirectory(obj).pipe(
       map((data: any) => {
-        if(this.selectedAlphabet == "All"){
-          // this.mentors = this.mentors.concat(data.result.data);
-          this.mentors = data.result.data;
-          this.mentorsCount = data.result.count;
-        }else{
-          data.result.data.forEach((ele:any) => {
-            if(ele.key == this.selectedAlphabet){
-              this.mentors = {}
-              this.mentors = ele;
-              console.log(this.mentors)
-            }
-          });
-          
-        }
-        
+        this.mentors = data.result.data;
+        this.mentorsCount = data.result.count;
       }))
   }
   eventAction(event:any) {
     switch (event.type) {
       case 'cardSelect':
-        this.router.navigate(["/mentor-profile"],{ state: { mentorID: event.data._id } })
+        this.router.navigate(["/mentor-profile"], { queryParams: { mentorID: event.data._id } })
         break;
     }
   }
   onClickAlphabet(a:any){
-console.log(a)
-this.selectedAlphabet = a;
-this.getMentor().subscribe()
+    this.selectedAlphabet = a;
+    if (this.selectedAlphabet == 'All') {
+      this.selectedMentors = this.mentors
+    } else {
+      this.mentors.forEach((ele: any) => {
+        if (ele.key == this.selectedAlphabet) {
+          this.selectedMentors = ele;
+        }
+      });
+    }
   }
 }
--- src/app/modules/layout/components/mentor-profile/mentor-profile.component.ts
+++ src/app/modules/layout/components/mentor-profile/mentor-profile.component.ts
@@ -1,56 +1,87 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MentorService } from 'src/app/core/services/mentor/mentor.service';
+import { SessionService } from 'src/app/core/services/session/session.service';
 
-
+interface item {
+  userId?: string;
+}
 @Component({
   selector: 'app-mentor-profile',
   templateUrl: './mentor-profile.component.html',
   styleUrls: ['./mentor-profile.component.scss']
 })
 export class MentorProfileComponent implements OnInit {
-mentorId :any;
-details: any = {
-  buttonLabel: "EDIT_PROFILE",
-  form: [
-    {
-      title: 'ABOUT',
-      key: 'about',
-    },
-    {
-      title: 'DESIGNATION',
-      key: 'designation',
-    },
-    {
-      title: 'YEAR_OF_EXPERIENCE',
-      key: 'experience',
-    },
-    {
-      title: "KEY_AREAS_OF_EXPERTISE",
-      key: "areasOfExpertise"
-    },
-    {
-      title: "EDUCATION_QUALIFICATION",
-      key: "educationQualification"
-    },
-    {
-      title: "LANGUAGES",
-      key: "languages"
-    }
-  ],
-  menteeForm: ['SESSIONS_ATTENDED'],
-  data: {},
-};
-layout= 'start start'
-  constructor(private router: Router, private mentorProfile:MentorService) { 
-    this.mentorId = this.router.getCurrentNavigation()?.extras.state;
-    
-  }
-
+  mentorId :any;
+  details: any = {
+    buttonLabel: "EDIT_PROFILE",
+    form: [
+      {
+        title: 'ABOUT',
+        key: 'about',
+      },
+      {
+        title: 'DESIGNATION',
+        key: 'designation',
+      },
+      {
+        title: 'YEAR_OF_EXPERIENCE',
+        key: 'experience',
+      },
+      {
+        title: "KEY_AREAS_OF_EXPERTISE",
+        key: "areasOfExpertise"
+      },
+      {
+        title: "EDUCATION_QUALIFICATION",
+        key: "educationQualification"
+      },
+      {
+        title: "LANGUAGES",
+        key: "languages"
+      }
+    ],
+    menteeForm: ['SESSIONS_ATTENDED'],
+    data: {},
+  };
+  layout= 'start start'
+  mentorDirectory = true;
+  mentorUpComingSession: Array<item> = [];
+  showLoadMoreButtonUpcomingSession: boolean = false;
+  page = 1;
+  limit = 1;
+  noData:any;
+  constructor(private router: Router, private route: ActivatedRoute, private mentorProfile: MentorService, private sessionService: SessionService) {
+    this.route.queryParams.subscribe(
+      params => {
+        this.mentorId = params;
+      }
+    )}
   ngOnInit(): void {
-    this.mentorProfile.getMentorDetails(this.mentorId.mentorID).subscribe((data:any)=>{
+    this.mentorProfile.getMentorDetails(this.mentorId.mentorID).subscribe((data: any) => {
       this.details.data = data;
+    })
+    this.getUpcomingSession()
+  }
+  getUpcomingSession() {
+    let obj = {
+      "id": this.mentorId.mentorID,
+      "page": this.page,
+      "limit": this.limit
+    }
+    this.sessionService.mentorUpComingSession(obj).subscribe((data: any) => {
+      this.mentorUpComingSession = this.mentorUpComingSession.concat(
+        data.result[0].data
+      )
+      this.showLoadMoreButtonUpcomingSession =
+        !(data.result.count == this.mentorUpComingSession.length)
     })
   }
 
+  buttonClick(event: any) {
+  }
+  onClickViewMoreUpcomingSessions() {
+    this.page = this.page + 1
+    this.getUpcomingSession()
+  }
 }
